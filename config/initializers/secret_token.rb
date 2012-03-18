d9bad307bbd2d57a04e4e8b42618db65ecdfe3b6
Karthick <affkar@gmail.com>
2012-03-18 19:55:01 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karapp::Application.config.secret_token = '28b7fe1cbf85583769977c996a3487f596d1544308cfbd97c25e30409adf673a57f82520d98c651283012802963857456ca2b3ab7e8bfef1068aaab8b494257e'
